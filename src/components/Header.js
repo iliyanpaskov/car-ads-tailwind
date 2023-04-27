092956429e8221497a30dea01ef8915d610053ea
iliyanpaskov <108199866+iliyanpaskov@users.noreply.github.com>
2023-04-27 14:00:59 +0300

    Updated Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,18 +9,18 @@ const Header = () => {
         <>
             <Logo />
             <section className="w-11/12 bg-gray-800 text-white flex justify-between items-center  sticky top-0  z-10">
-                <article className=" bg-blue-900 w-1/5 h-full p-2 flex items-center">
-                    <p className="text-white text-2xl h-full">
+                <article className=" bg-blue-900 w-1/4 h-full p-2 flex items-center">
+                    <p className="text-white text-2xl h-full p-2">
                         Explore 2023
                         Mustang Mach-E &#174;
                     </p>
-                    <i className="fa-solid fa-angles-down p-3 text-3xl "></i>
+                    <Link to='/'>  <i className="fa-solid fa-house fa-fade p-3 text-3xl my-4"></i></Link>
                 </article>
 
                 <article className="w-1/2 ">
                     <ul className="flex flex-row justify-around p-3 place-items-center">
                         <li className="text-lg">
-                            <Link to='/prices'>Gallery</Link>
+                            <Link to='/gallery'>Gallery</Link>
                         </li>
                         <li className="text-lg">
                             <Link to='/prices'>Price list</Link>
