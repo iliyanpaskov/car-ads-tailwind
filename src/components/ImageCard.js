06fe73856fb1425d5630221287e8b72b4f5d706f
iliyanpaskov <108199866+iliyanpaskov@users.noreply.github.com>
2023-04-29 02:36:09 +0300

    Gallery Modal created

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -1,9 +1,30 @@
+import { useState } from "react";
+import PhotoModal from "./PhotoModal";
+
 const ImageCard = (image) => {
+    const [openModal, setOpenModal] = useState(false);
+    const picture = image.image;
+
+    const closeHandler = () => {
+        setOpenModal(false);
+    }
+
+    const showModal = () => {
+        setOpenModal(true);
+        // console.log(picture);
+    }
 
     return (
-        <section className="w-80">
-            <img className="w-full max-h-44 object-cover cursor-pointer hover:scale-110 duration-500 shadow-md shadow-slate-500 rounded-sm" src={`${image.image}`} alt="car" />
-        </section>
+        <>
+            <section className="w-80">
+                <img onClick={showModal} className="w-full max-h-44 object-cover cursor-pointer hover:scale-110 duration-500 shadow-md shadow-slate-500 rounded-sm" src={`${picture}`} alt="car" />
+            </section>
+            <PhotoModal
+                open={openModal}
+                picture={picture}
+                onClose={() => closeHandler()}
+            />
+        </>
     )
 }
 
