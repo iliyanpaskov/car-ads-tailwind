c171024aa5b6b7feb5f1bdc416338df86fd9d326
iliyanpaskov <108199866+iliyanpaskov@users.noreply.github.com>
2023-04-28 12:38:43 +0300

    Page not found created

--- src/App.js
+++ src/App.js
@@ -4,16 +4,18 @@ import Home from "./components/Home";
 import Footer from "./components/Footer";
 import WhyMustang from "./components/WhyMustang";
 import GetNotifications from "./components/GetNotifications";
+import NotFound from "./components/NotFound";
 
 function App() {
   return (
     <div className="min-h-screen bg-slate-200 flex items-center flex-col font-sans">
       <Header />
-      <main className="w-11/12 max-w-7xl">
+      <main className="w-11/12 max-w-7xl min-h-screen">
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/mustang" element={<WhyMustang />} />
-          <Route path="//get-notifications" element={<GetNotifications />} />
+          <Route path="/get-notifications" element={<GetNotifications />} />
+          <Route path="/*" element={<NotFound />} />
         </Routes>
 
       </main>
